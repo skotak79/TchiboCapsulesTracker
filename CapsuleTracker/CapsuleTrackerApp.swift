e105c9c9d399479e2a0c0c90b75fc8f6200f7cca
Wojtek Skotak <wojtek.skotak@gmail.com>
2023-10-16 20:43:51 +0200

    Initial Commit

---
+++ CapsuleTracker/CapsuleTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapsuleTrackerApp.swift
+//  CapsuleTracker
+//
+//  Created by Wojtek on 16/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct CapsuleTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
